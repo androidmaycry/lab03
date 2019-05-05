c1646e2ae32e493d5bd08fe7d0fe1ccd2c309f3f
gallottino <d.gallotti@outlook.it>
2019-05-05 19:09:14 +0200

    Add Registration

--- Lab03/riders/src/main/java/com/mad/riders/MainActivity.java
+++ Lab03/riders/src/main/java/com/mad/riders/MainActivity.java
@@ -1,156 +1,65 @@
 package com.mad.riders;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
-
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.net.Uri;
-import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.design.widget.BottomNavigationView;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.util.Log;
-import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import java.io.File;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
-class Dish{
-    private String description;
-    private int price;
+import org.w3c.dom.Text;
 
-    public Dish() {
 
-    }
-
-    public Dish(String description, int price) {
-        this.description = description;
-        this.price = price;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-}
-
-public class MainActivity extends AppCompatActivity
-        implements DailyOffer.OnFragmentInteractionListener, Reservation.OnFragmentInteractionListener, Home.OnFragmentInteractionListener, Profile.OnFragmentInteractionListener{
-
-    private static final String CheckPREF = "First Run";
-    private Profile profile;
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = item ->  {
-
-        switch (item.getItemId()) {
-            case R.id.navigation_home:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new Home()).commit();
-                return true;
-            case R.id.navigation_profile:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new Profile()).commit();
-                return true;
-            case R.id.navigation_dailyoffer:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new DailyOffer()).commit();
-                return true;
-            case R.id.navigation_reservation:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new Reservation()).commit();
-                return true;
-        }
-        return false;
-    };
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                    new Home()).commit();
-        }
-    }
+        setContentView(R.layout.activity_nav_app);
 
-    @Override
-    public void onFragmentInteraction(Uri uri) {
-
-    }
-
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        SharedPreferences first_check = getSharedPreferences(CheckPREF, 0);
-        Log.d("MAIN","resuming");
-
-        if(first_check.getBoolean("firsRun",false)){
-            first_check.edit().clear().commit();
-            goHome();
-        }
-    }
-
-    public void goHome() {
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new Home()).commit();
-        BottomNavigationView navigationView = (BottomNavigationView) findViewById(R.id.navigation);
-        navigationView.getMenu().getItem(3).setChecked(true);
-        navigationView.getMenu().getItem(2).setChecked(false);
-        navigationView.getMenu().getItem(1).setChecked(false);
-        navigationView.getMenu().getItem(0).setChecked(false);
-    }
-
-    /*private final String DISHES_PATH = "dishes/";
-
-    FirebaseDatabase database = FirebaseDatabase.getInstance();
-    DatabaseReference myRef = database.getReference(DISHES_PATH);
-    Map<String, Object> test = new HashMap<String, Object>();
-
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        findViewById(R.id.button).setOnClickListener(e -> {
-            test.put("carbonara", new Dish("pasta con porco dio", 22));
-            myRef.updateChildren(test);
-        });
-
-        // Read from the database
-        myRef.addValueEventListener(new ValueEventListener() {
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        auth.addAuthStateListener(new FirebaseAuth.AuthStateListener() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-                List<Dish> dishes = new ArrayList<>();
-                for(DataSnapshot d : dataSnapshot.getChildren())
-                    dishes.add(d.getValue(Dish.class));
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
 
-                for(Dish d : dishes)
-                    Log.d("MAIN", "Value is: " + d.getDescription());
-            }
-
-            @Override
-            public void onCancelled(DatabaseError error) {
-                // Failed to read value
-                Log.w("MAIN", "Failed to read value.", error.toException());
             }
         });
-    }*/
+
+
+        TextView email = findViewById(R.id.email);
+        TextView password = findViewById(R.id.password);
+
+        findViewById(R.id.sign_up).setOnClickListener(e -> {
+            Intent i = new Intent(this, EditProfile.class);
+            startActivity(i);
+        });
+
+        findViewById(R.id.sign_in).setOnClickListener(e -> {
+            auth.signInWithEmailAndPassword(email.getText().toString(), password.getText().toString())
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d("success", "signInWithEmail:success");
+
+
+                                Intent i = new Intent(MainActivity.this,NavApp.class);
+                                i.putExtra("UID",auth.getUid().toString());
+                                startActivityForResult(i,10);
+                            } else {
+                                Toast.makeText(MainActivity.this,"Wrong Username or Password",Toast.LENGTH_LONG);
+                            }
+                        }
+                    });
+        });
+
+    }
 }
